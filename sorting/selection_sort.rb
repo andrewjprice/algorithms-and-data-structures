602e78f5661fb2fbbef73e1ed4dd0639eabd0c95
Andrew <andrew.j.price@outlook.com>
2019-02-18 19:04:44 -0700

    Add Selection Sort

--- sorting/selection_sort.rb
+++ sorting/selection_sort.rb
@@ -1,6 +1,6 @@
 require_relative 'sort.rb'
 
-class InsertionSort
+class SelectionSort
     include Sort
 
     attr_reader :set
@@ -13,11 +13,13 @@ class InsertionSort
         n = set.size - 1
         
         n.times do |i|
+            min = i
             (i+1).upto(n) do |j|
-                swap(set, i, j) if !less(set, i, j)
+                min = j if less(set, j, min)
             end
+            swap(set, i, min)
         end
-        
+
         set
     end
 end
---
+++ sorting/spec/selection_sort_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../selection_sort.rb'
+
+describe SelectionSort do
+    subject { described_class.new(unsorted_list) }
+
+    let(:unsorted_list) { [2,1,3,5,4] }
+    let(:sorted_list) { [1,2,3,4,5] }
+
+    describe '#sort' do
+        it 'sorts list in asc order' do
+            expect(subject.sort).to eq sorted_list
+        end
+    end
+end
