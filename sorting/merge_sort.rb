c1547e45885b22bdc4ef459efb0aa4b81d047411
Andrew <andrew.j.price@outlook.com>
2019-02-18 19:26:25 -0700

    Add Merge Sort

---
+++ sorting/merge_sort.rb
@@ -0,0 +1,27 @@
+require_relative 'sort.rb'
+
+class MergeSort
+    include Sort
+
+    attr_reader :set
+
+    def initialize(set)
+        @set = set
+    end
+
+    def sort
+        sort_list(set)
+    end
+
+    private
+
+    def sort_list(list)
+        return list if list.size <= 1
+        high = list.size - 1
+        mid = high / 2
+        left = sort_list(list[0..mid])
+        right = sort_list(list[mid+1..high])
+
+        merge(left, right)
+    end
+end
